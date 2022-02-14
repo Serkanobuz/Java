05138ab7934acd6086eb72412e0794b5115dcb68
Serkanobuz <99622622+Serkanobuz@users.noreply.github.com>
2022-02-13 16:52:11 -0800

    Update

---
+++ variables/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int inventoryNumber = 12;
+        String message = "Inventory : ";
+        System.out.println( message + inventoryNumber);
+        System.out.println("Inventory : "+inventoryNumber);
+        System.out.println("Inventory : "+inventoryNumber);
+        System.out.println("Inventory : "+inventoryNumber);
+    }
+}
